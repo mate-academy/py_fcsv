31238cad62a4ade1bd1e46d9a30ee228a3c51965
ValentinaLV <tina.lysenok@gmail.com>
2019-12-02 14:14:32 +0200

    hw11_py_calculate_costs

--- fcsv.py
+++ fcsv.py
@@ -1,3 +1,23 @@
-def calc_price(filename, open=open):
+"""
+module calculate costs of products from csv file
+"""
+import csv
+from functools import reduce
 
-    return 42.0
+
+def calc_price(filename: str, open_=open) -> float:
+    """
+    There is a CSV file containing data in this format:
+    Product name, price, quantity
+    Calculate total cost for all products (price * quantity).
+    :param filename: str
+    :param open_: open
+    :return: float
+    """
+    with open_(filename, 'rt') as file:
+        sum_prod_costs = 0.0
+        csv_reader = csv.reader(file, delimiter=',')
+        param_lsts = [list(map(float, line[1:])) for line in csv_reader]
+        for lst in param_lsts:
+            sum_prod_costs += reduce((lambda x, y: x * y), lst)
+    return sum_prod_costs
