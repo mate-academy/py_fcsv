cc49ae482297248f4f1a0bfe950789305ede0835
Artem <artger@ukr.net>
2019-12-02 13:43:09 +0200

    version 1

--- fcsv.py
+++ fcsv.py
@@ -1,3 +1,19 @@
-def calc_price(filename, open=open):
+'''
+Module
+'''
 
-    return 42.0
+
+def calc_price(read_file, open_=open):
+    '''
+    Calculate total cost for all products.
+    :param read_file:
+    :param open:
+    :return:
+    '''
+    with open_(read_file, "rt") as reading:
+        result = 0
+        for line in reading:
+            line = line.split(',')
+            price, count = float(line[1]), float(line[2])
+            result += price * count
+    return result
