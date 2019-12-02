42d007b3f27753cbc6218e47879a07da533f2fbe
d1makov <makovskyj.danylo@gmail.com>
2019-12-02 17:44:47 +0200

    final

--- fcsv.py
+++ fcsv.py
@@ -1,3 +1,14 @@
-def calc_price(filename, open=open):
+"""
+docstring
+"""
+import pandas as pd
 
-    return 42.0
+
+def calc_price(filename):
+    """
+
+    :param filename:
+    :return:
+    """
+    file = pd.read_csv(filename, names=["name", "price", "quantity"])
+    return sum(file['price'] * file['quantity'])
--- test_fcsv.py
+++ test_fcsv.py
@@ -1,9 +1,16 @@
+"""
+docstring
+"""
 import io
 import fcsv
 
 
 def test_fcsv():
+    """
+
+    :return:
+    """
     s = io.StringIO("""Apples,2.3,5
-Cherrys,7.2,4
-Tomatoes,3.4,10""")
-    assert fcsv.calc_price(s, lambda x,m: x) == 74.3
+                        Cherrys,7.2,4
+                        Tomatoes,3.4,10""")
+    assert fcsv.calc_price(s, lambda x, m: x) == 74.3
