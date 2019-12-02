6a68dec67934842d648e76103e203832a1cf5bf7
koreyo <KoretsValentyn@gmail.com>
2019-12-02 20:48:55 +0200

    solv

--- fcsv.py
+++ fcsv.py
@@ -1,3 +1,12 @@
-def calc_price(filename, open=open):
+"""There is a CSV file containing data in this format: Product name, price, quantity"""
 
-    return 42.0
+
+def calc_price(filename, opens=open):
+    """Calculate total cost for all products """
+    cost_all_item = 0
+    with opens(filename, 'rt') as csv_file:
+        for row in csv_file.readlines():
+            item = row.split(",")
+            cost_all_item = (float(item[1]) * float(item[2])) + cost_all_item
+    csv_file.close()
+    return cost_all_item
