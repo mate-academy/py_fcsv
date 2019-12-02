669a72d8b0bad9099d654c88fff54c2442fd95c6
Dima <dima.sapsay@gmail.com>
2019-12-02 12:16:38 +0200

    first commit

--- fcsv.py
+++ fcsv.py
@@ -1,3 +1,14 @@
-def calc_price(filename, open=open):
+"""
+There is a CSV file containing data in this format:
+Product name, price, quantity.
+Calculate total cost for all products.
+"""
 
-    return 42.0
+
+def calc_price(filename, open_=open):
+    """Calculate total cost for all products"""
+    total_cost = 0
+    with open_(filename, 'rt') as rfile:
+        for line in rfile.readlines():
+            total_cost += float(line.split(',')[1]) * int(line.split(',')[2])
+    return total_cost
