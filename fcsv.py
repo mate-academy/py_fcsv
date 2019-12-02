0b75e5f61fb8e43d5edc4f5ba52a87330433ecd9
DukhDmytro <cowboybebop4991@gmail.com>
2019-12-02 12:17:18 +0200

    1st commit

--- fcsv.py
+++ fcsv.py
@@ -1,3 +1,10 @@
-def calc_price(filename, open=open):
+"""Reader from csv module return a reader object
+which will iterate over lines in the given csv file"""
+from csv import reader
 
-    return 42.0
+
+def calc_price(filename, opener=open):
+    """return total cost for all products in csv file"""
+    with opener(filename, "rt") as csvfile:
+        csvreader = reader(csvfile)
+        return sum(float(row[1]) * float(row[2]) for row in csvreader)
