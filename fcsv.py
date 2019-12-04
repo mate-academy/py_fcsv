7ab6e349c8416c34108ea89b52b262cf79d971b2
Platoniuks <platon@alterpost.org>
2019-12-04 11:02:57 +0200

    resolved

--- fcsv.py
+++ fcsv.py
@@ -1,3 +1,18 @@
-def calc_price(filename, open=open):
+"""Module for reading CSV file and calculations
+"""
+import csv
 
-    return 42.0
+
+def calc_price(filename, open_=open):
+    """
+    Get the total bill amount
+    :param filename: CSV file
+    :param open_: function to open file, default - built-in open()
+    :return: result of calculations
+    """
+    result = 0
+    with open_(filename, 'r') as bill:
+        bill = csv.reader(bill, delimiter=',')
+        for i in bill:
+            result += float(i[1]) * float(i[2])
+    return result
