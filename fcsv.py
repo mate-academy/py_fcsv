de278490edf78f92e1aca68134bdb6c95a6a2cd4
alexander <earthhuman41@gmail.com>
2019-12-21 20:48:49 +0200

    csv

--- fcsv.py
+++ fcsv.py
@@ -1,3 +1,16 @@
-def calc_price(filename, open=open):
+"""
+calc_price
+"""
 
-    return 42.0
+
+def calc_price(filename, open_=open):
+    """
+    calc_price
+    """
+    with open_(filename, 'r') as file1:
+        result = 0
+        for line in file1:
+            prepared_line = line.split(',')
+            result += float(prepared_line[1]) * float(prepared_line[2])
+
+    return result
